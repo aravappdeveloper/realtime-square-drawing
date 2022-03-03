5d95e72a26b7f086fc5a37a8be44659535cfe649
aravappdeveloper <74691700+aravappdeveloper@users.noreply.github.com>
2022-03-02 18:32:47 -0600

    Add files via upload
    Project finished! It can now change properties of the square in real-time!

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +17,11 @@ function setup(){
 
 function draw(){
     background("#969A97");
+
+    document.getElementById("square_sides").innerHTML = "Side length of square: " + difference + "px";
+    fill('#F90093');
+    stroke('F90093');
+    square(noseX, noseY, difference);
 }
 
 function modelloaded(){
@@ -19,6 +30,16 @@ function modelloaded(){
 
 function gotPoses(results){
     if(results.length > 0){
+        // nose
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("NoseX = " + noseX + ", NoseY = " + noseY);
+        // wrists
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("LeftWristX = " + leftWristX + ", RightWristX = " + rightWristX);
+        console.log("Difference: " + difference);
     }
 }
